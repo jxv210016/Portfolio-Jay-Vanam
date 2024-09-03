f9f90c117b9f064ee2d855ac8abcf32d25d82dfa
jxv210016 <123779263+jxv210016@users.noreply.github.com>
2024-09-02 21:51:27 -0500

    blog

---
+++ blog/script.js
@@ -0,0 +1,10 @@
+function toggleSearch() {
+  var searchBar = document.getElementById("searchBar");
+  if (searchBar.style.display === "none" || searchBar.style.display === "") {
+    searchBar.style.display = "block";
+  } else {
+    searchBar.style.display = "none";
+  }
+}
+
+// Placeholder for additional JavaScript functionalities
